302dc98d73d59b1c1474432f7febbc1dd999266d
asakuno <takahiro.asakuno@gmail.com>
2022-06-17 02:13:25 +0900

    create_board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -7,4 +7,25 @@ class BoardsController < ApplicationController
     @board = Board.new
   end
 
+  def create
+    @board = Board.new(board_params)
+    @board.user_id = current_user.id
+    if @board.save
+      redirect_to boards_path, success: (t"defaults.message.created", item: Board.model_name.human)
+    else
+      flash.now[:danger] = (t"defaults.message.not_created", item: Board.model_name.human)
+      render :new
+    end 
+  end
+
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :body)
+  end
+
 end
